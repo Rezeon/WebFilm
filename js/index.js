2ed4dc2b68d241c0b65d4947cfc048ba64fd7ad0
Rezeon <Rheynoair@gmail.com>
2025-01-01 12:13:55 +0700

    perubahan nama

--- js/index.js
+++ js/index.js
@@ -10,24 +10,3 @@ const dropdownButton = document.getElementById('dropdownButton');
                 dropdownContent.classList.add('hidden');
             }
         });
-
-// Fungsi untuk toggle dropdown
-function toggleDropdown(id) {
-    const dropdown = document.getElementById(id);
-    dropdown.classList.toggle('hidden');  // Menampilkan atau menyembunyikan dropdown
-  }
-  
-  // Toggle menu dropdown saat tombol hamburger di klik
-  document.getElementById('hamburger-button').addEventListener('click', function () {
-    const dropdownMenu = document.getElementById('dropdown-menu');
-    dropdownMenu.classList.toggle('hidden');  // Menampilkan atau menyembunyikan menu dropdown utama
-  });
-  
-  // Menambahkan event listener untuk membuka genre atau bantuan film dropdown
-  document.querySelectorAll('.w-full.text-left.text-sm.hover\\:text-gray-400').forEach(button => {
-    button.addEventListener('click', function () {
-      const dropdownId = this.innerText === "Genre" ? 'genreDropdown' : 'helpDropdown';
-      toggleDropdown(dropdownId);
-    });
-  });
-  
